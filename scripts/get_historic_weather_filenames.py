3d174d1ded8ad0f4882e9bf096083f893eed1563
Chris Kirney <chris.kirney@canada.ca>
2023-06-30 08:39:18 -0400

    Updating to provide list of historic and future weather filenames for easier reference as well as scripts to provide these.

--- scripts/get_historic_weather_filenames.py
+++ scripts/get_historic_weather_filenames.py
@@ -5,9 +5,6 @@ f = open("./Can_Canada_Historic_files.txt", "r")
 
 names = list()
 
-pre_string = "https://climate.onebuilding.org/WMO_Region_4_North_and_Central_America/CAN_Canada/"
-pre_string_len = len(pre_string)
-
 for line in f:
     line_length = len(line)
     line_start = 0
@@ -23,13 +20,12 @@ for line in f:
             end_name = start_index + index_close
             line_start = end_name + 1
             name = line[(start_index):end_name]
-            name_add = pre_string + name
-            if (name_add in names) or (len(name) == 0):
+            if (name in names) or (len(name) == 0):
                 continue
             else:
-                zip_loc = name_add.find("zip")
+                zip_loc = name.find("zip")
                 if zip_loc >= 0:
-                    names.append(name_add)
+                    names.append(name)
         else:
             line_start = line_length + 10
 
@@ -37,23 +33,5 @@ sort_names = sorted(names)
 
 f.close()
 json_object = json.dumps(sort_names, indent=4)
-with open("./download_historic_names.json","w") as outfile:
+with open("../historic_weather_filenames.json","w") as outfile:
     outfile.write(json_object)
-
-count_ind = 0
-
-for name in sort_names:
-    if len(name) == 0:
-        continue
-    else:
-        count_ind += 1
-        out_name_start = name[pre_string_len:len(name)]
-        start_index = out_name_start.find("/") + 1
-        out_name = ".././historic/" + out_name_start[start_index:len(out_name_start)]
-        r = requests.get(name, allow_redirects=True)
-        open(out_name, 'wb').write(r.content)
-
-print(count_ind)
-
-#print("hello")
-#https://climate.onebuilding.org/WMO_Region_4_North_and_Central_America/CAN_Canada/AB_Alberta/CAN_AB_Athabasca.AgCM.712710_TMYx.2004-2018.zip
